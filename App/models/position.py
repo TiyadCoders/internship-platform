da9d03ed07a94b1fa98fdd98126b1c836e8a3f4d
soniarosem <soniamohammedb2b@gmail.com>
2025-11-26 21:42:06 -0400

    Finished refactoring position class, controllers, views and tests

--- App/models/position.py
+++ App/models/position.py
@@ -11,20 +11,23 @@ class Position(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     number_of_positions = db.Column(db.Integer, default=1)
+    description = db.Column(db.Text, nullable=True)
     status = db.Column(Enum(PositionStatus, native_enum=False), nullable=False, default=PositionStatus.open)
     employer_id = db.Column(db.Integer, db.ForeignKey('employer.id'), nullable=False)
     employer = db.relationship("Employer", back_populates="positions")
 
-    def __init__(self, title, employer_id, number):
+    def __init__(self, title, employer_id, number, description=None):
         self.title = title
         self.employer_id = employer_id
         self.status = PositionStatus.open
         self.number_of_positions = number
+        self.description = description
 
     def toJSON(self):
         return {
             "id": self.id,
             "title": self.title,
+            "description": self.description,
             "number_of_positions": self.number_of_positions,
             "status": self.status.value,
             "employer_id": self.employer_id
